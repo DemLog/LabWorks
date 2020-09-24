25f50ab62deac119cc1fae0412d0fd68e34c5b8c
DemLoveSky <dem.loginov@gmail.com>
2020-09-25 02:34:16 +0500

    not see!!!

---
+++ labs02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+
+namespace labs02
+{
+    class PieceMove
+    {
+        public int figure; 
+        public string x0y0;    
+        public string xy;
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            // создание класса
+            string figure;
+            while (true)
+            {
+                figure = Console.ReadLine();
+                // проверка на число
+                // проверка на корректность
+                break;
+            }
+
+            string x0y0;
+            string xy;
+            while (true)
+            {
+                x0y0 = Console.ReadLine();
+                xy = Console.ReadLine();
+                // проверка на корректность
+                // проверка на границы
+                break;
+            }
+            Boolean response = true;// вызов функции проверки возможности хода
+            if (response)
+            {
+                // ваш ход правилен
+            }
+            else
+            {
+                // ваш ход неправилен, учись играть
+            }
+
+            
+        }
+        static Boolean Correct_Check(string data)
+        {
+            return true;
+        }
+        static Boolean Correct_Check(int data)
+        {
+            return true;
+        }
+
+    }
+}
